3418d50cbe2bf357d63bf7657263695a8daba0da
wallacesintra <wallvxwallace962@gmail.com>
2024-01-27 20:59:22 +0300

    ui

--- app/src/main/java/com/example/supees/MainActivity.kt
+++ app/src/main/java/com/example/supees/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.supees.data.HeroesRepository
+import com.example.supees.model.Hero
 import com.example.supees.ui.theme.SupeesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +28,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppLayout()
                 }
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge,
+            )
+        },
         modifier = modifier
     )
 }
+@Composable
+fun AppLayout(){
+//    HeroList(heroList = HeroesRepository.heroes)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar()
+        }
+    ) {
+        /* Important: It is not a good practice to access data source directly from the UI.
+        In later units you will learn how to use ViewModel in such scenarios that takes the
+        data source as a dependency and exposes heroes.
+         */
+        val heroes = HeroesRepository.heroes
+        HeroList(heroList = heroes, contentPadding = it)
+    }
 
-@Preview(showBackground = true)
+}
+
+
+
+
+@Preview()
 @Composable
 fun GreetingPreview() {
     SupeesTheme {
-        Greeting("Android")
+        HeroList(heroList = HeroesRepository.heroes)
     }
 }
